332f8b5affaf0724aac87ad17dad5a0b71989131
Miles Krusniak <miles.krusniak@yale.edu>
2019-10-26 12:21:29 -0400

    Remove emoji from tweets

--- twitter_prototype.py
+++ twitter_prototype.py
@@ -1,11 +1,4 @@
 from twython import Twython
-from google.cloud import language
-from google.cloud import automl_v1beta1 as automl
-import nltk
-from nltk.sentiment import SentimentAnalyzer
-from nltk.sentiment.util import *
-import sys
-
 
 twitter_credentials = {
     'client_key_secret': 'q3LWUS9zrpazVcJ2pYEzXG71DJ43XYsiZxcIRVoskmIH99Bjq5',
@@ -14,15 +7,7 @@ twitter_credentials = {
     'access_token': '1187917483300261891-Lr3IUJQTdqxUymbZnTz75Eq7rWGNyQ'
 }
 
-
 twitter = Twython(twitter_credentials['client_key'], twitter_credentials['client_key_secret'])
-google_automl = automl.AutoMlClient()
-google_predictions = automl.PredictionServiceClient()
-
-model_full_id = google_automl.model_path(
-    'yhackadroit', 'us-east4', 'model_id'
-)
-
 
 query = twit_params = {
     'q': "JetBlue -filter:retweets",
@@ -37,36 +22,13 @@ r = twitter.search(**query)
 tweet_text = ''
 tweets = []
 for tweet in r['statuses']:
-    tweet_text += tweet['full_text'].replace("\n", " ").replace("\"", "\\\"") + '\n'
+    tweet_text += tweet['full_text'] \
+                      .replace("\n", " ") \
+                      .replace("\"", "\\\"") \
+                      .replace("[\uD83C\uDF00-\uDFFF\uD83D\uDC00-\uDDFF]", "") \
+                      .replace("@", "") \
+                  + '\n'
     tweets += [tweet_text]
 
 with open("tweets.txt", "a") as tweets_file:
     tweets_file.write(tweet_text)
-
-
-
-payload = {'text_snippet': {'content': tweet_text, 'mime_type': 'text/plain'}}
-response = google_predictions.predict(model_full_id, payload, {})
-
-
-for result in response.payload:
-    print("Predicted sentiment label: {}".format(result.text_sentiment.sentiment))
-
-for key, data in response.metadata.items():
-    if key == 'sentiment_score':
-        print("Normalized sentiment score: {}".format(data))
-
-
-# training_data = [
-#     (['test', 'which', 'is', 'tokenized'], 'positive')
-# ]
-#
-# analyzer = SentimentAnalyzer()
-#
-# for tweet in tweets:
-#     unigram_feats = analyzer.unigram_word_feats(
-#         analyzer.all_words([mark_negation(doc) for doc in training_data]), min_freq=3)
-#     analyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
-#
-
-
